4524732c9baeddbfaa8130b62fd32c0e92e1998f
Ferdinand Prantl <prantlf@gmail.com>
2020-04-25 18:41:48 +0200

    Allow testing with Firefox using en environment with BROWSER=firefox

--- test/createClient.js
+++ test/createClient.js
@@ -3,10 +3,11 @@
 var wd = require('wd');
 
 var automatedBrowsers = require('./automatedBrowsers.json');
+var browser = process.env.BROWSER || 'chrome'
 
 var createClient = function createClient() {
 	var res = wd.promiseChainRemote('localhost', process.env.LOCAL_SELENIUM_PORT || 4444)
-		.init(automatedBrowsers.chrome.driver.config)
+		.init(automatedBrowsers[browser].driver.config)
 		.setWindowSize(1000, 1000) // do something
         .quit();
 	return res;
